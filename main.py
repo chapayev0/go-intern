1c66cddf0464e71ca9559d3844d7c56902b59943
chapayev0 <41099408+chapayev0@users.noreply.github.com>
2023-02-19 17:09:04 +0530

    create ui

---
+++ main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import sys
+from PySide2 import QtGui, QtCore, QtWidgets
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+
+from ui.ui_main import *
+
+import os
+
+class main_ui(QMainWindow):
+
+    def __init__(self):
+
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    window = main_ui()
+
+    window.show()
+    sys.exit(app.exec_())
